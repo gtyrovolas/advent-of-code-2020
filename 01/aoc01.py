9db08491b053dab457d5cb44c84e4de9ef0a01da
Giannis Tyrovolas <johntyro@hotmail.com>
2020-12-13 21:19:29 +0200

    completed 01b & refactored 01a

--- 01/aoc01.py
+++ 01/aoc01.py
@@ -1,20 +1,32 @@
 
-def main():
-    file_in = open('01.in', "r")
-    nums = [int(num) for num in file_in]
-    nums.sort()
+file_in = open('01.in', "r")
+nums = [int(num) for num in file_in]
+nums.sort()
+
+def find(target, nums):
     le, ri = 0, len(nums) - 1
     
     while le < ri:
         current_sum = nums[le] + nums[ri]
-        if current_sum < 2020:
+        if current_sum < target:
             le += 1
-        elif current_sum > 2020:
+        elif current_sum > target:
             ri -= 1
         else:
             break
     else:
-        raise RuntimeError
-    
-    return nums[le] * nums[ri]
+        return None
+    return nums[le], nums[ri]
 
+def original():
+    fi, sec = find(2020, nums)
+    return fi * sec, (fi, sec)
+
+def optional():
+    for i, fi in enumerate(nums):
+        result = find(2020 - fi, nums[:i] + nums[i+1 :])
+
+        if result is not None:
+            sec, thi = result[0], result[1]
+            return fi * sec * thi, (fi, sec, thi)    
+    
